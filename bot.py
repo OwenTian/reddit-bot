c7cf439f18af245173aebd6c4ac0792d2ff29bd9
OwenTian <owen.tian@gmail.com>
2017-11-20 18:13:11 -0500

    add run_bot function

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ PRAW - wrapper for reddit API, makes accessing reddit a lot easier
 
 import praw
 import config
-
+import time
 
 def bot_login():
+	print "Logging in..."
 	r = praw.Reddit(username = config.username,
 		password = config.password,
 		client_id = config.client_id,
@@ -17,7 +18,20 @@ def bot_login():
 
 	return r
 
+def run_bot(r):
+	print "Obtaining comments"
+	for comment in r.subreddit('test').comments(limit=25):
+		if "Wyrd Reconstruction!" in comment.body:
+			print "\"Wyrd Reconstruction\" found!"
+			print "replied to comment " + comment.id
+			comment.reply("CRIT! 0. Bleed.")
+
+	print "Sleeping for 10 seconds"
+	time.sleep(10)
+
 r = bot_login()
+while True:
+	run_bot(r)
 
 
 
