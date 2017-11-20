cbbb12809b9f886000310e5e084fafe7083a45be
OwenTian <owen.tian@gmail.com>
2017-11-20 17:25:38 -0500

    add login code

--- bot.py
+++ bot.py
@@ -5,11 +5,19 @@ PRAW - wrapper for reddit API, makes accessing reddit a lot easier
 '''
 
 import praw
+import config
 
 
+def bot_login():
+	r = praw.Reddit(username = config.username,
+		password = config.password,
+		client_id = config.client_id,
+		client_secret = config.client_secret,
+		user_agent = "fluttertest's occultist heal responder")
 
-print("HI! I'm a bot!")
-print("I will be posting on reddit")
+	return r
+
+r = bot_login()
 
 
 
---
+++ config.py
@@ -0,0 +1,4 @@
+username = "fluttertest"
+password = "darkestdungeon"
+client_id = "VJka_8t8UssOJw"
+client_secret = "f2c8fgHxMz1DjDGwHCNvNtKH4JU"
