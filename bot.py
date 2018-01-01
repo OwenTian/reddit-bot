b76f3019dbac4b28470062542aeda5a4e980cf60
OwenTian <owen.tian@gmail.com>
2018-01-01 15:55:40 -0500

    add creation of file to log comments replied to

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ PRAW - wrapper for reddit API, makes accessing reddit a lot easier
 import praw
 import config
 import time
+import os
 
 def bot_login():
 	print "Logging in..."
@@ -21,27 +22,36 @@ def bot_login():
 def run_bot(r):
 	print "Obtaining comments"
 
-	for comment in r.subreddit('test').comments(limit=10):
-		if "Wyrd Reconstruction!" in comment.body and comment.id not in comments_replied_to and not comment.author == r.user.me():
+	for comment in r.subreddit('test').comments(limit=30):
+		if "Wyrd Reconstruction!" in comment.body and comment.id not in comments_replied_to:
+		# and not comment.author == r.user.me():
 			print "\"Wyrd Reconstruction\" found!"
 			comment.reply("CRIT! 0. Bleed.")
 			print "replied to comment " + comment.id
 
 			comments_replied_to.append(comment.id)
 
+			with open ("comments_replied_to.txt", "a") as f:
+				f.write(comment.id + "\n")
+
 	print "Sleeping for 10 seconds"
 	time.sleep(10)
 
 
 def get_saved_comments():
-	with open("comments_replied_to.txt", "r") as f:
-		comments_replied_to = f.read()
-		comments_replied_to = comments_replied_to.split("\n")
+	if not os.path.isfile("comments_replied_to.txt"):
+		comments_replied_to = []
 
+	else:
+		with open("comments_replied_to.txt", "r") as f:
+			comments_replied_to = f.read()
+			comments_replied_to = comments_replied_to.split("\n")
+
+	return comments_replied_to
 
 
 r = bot_login()
-comments_replied_to = []
+comments_replied_to = get_saved_comments()
 while True:
 	run_bot(r)
 
